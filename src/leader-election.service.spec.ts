1b9e5eab7a27df5da844b731100fd56653244b72
Dan Shapir <dan@precise.credit>
2023-11-06 15:16:41 +0200

    Add Jest testing and update TypeScript config

---
+++ src/leader-election.service.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { LeaderElectionService } from "./leader-election.service";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import {
+  KubeConfig,
+  CoordinationV1Api,
+} from "@kubernetes/client-node";
+import { mock } from 'jest-mock-extended';
+
+
+describe("LeaderElectionService", () => {
+  let service: LeaderElectionService;
+  
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LeaderElectionService,
+        EventEmitter2, // Assuming we're using EventEmitter2 directly
+        {
+          provide: "LEADER_ELECTION_OPTIONS",
+          useValue: {
+            leaseName: "test-lease",
+            namespace: "test-namespace",
+            renewalInterval: 10000,
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<LeaderElectionService>(LeaderElectionService);
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+});
